4bfa0ca98c85b905c291f6c6711f6b1b6fb68d98
Fernanda <mangorifernanda@gmail.com>
2023-08-30 18:46:32 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { Card, Typography } from "@material-tailwind/react";
+import Gedung from './component/gedung';
+
+const App = () => {
+    const [data, setData] = useState([])
+    const TABLE_HEAD = ["id", "id Fakultas", "Nama Resmi", "Nama Singkat", ""];
+    const fetchPosts = async () => {
+        await axios.get('https://api.codefm.my.id/fakultas/'
+        ).then(function (response) {
+            setData(response.data)
+
+        })
+    }
+
+    useEffect(() => {
+        fetchPosts()
+
+    }, [])
+
+    return (
+
+        <Card className=" box-border p-4 border-4 overflow-scroll">
+            <table className="align-center w-full min-w-max table-auto text-left">
+                <thead>
+                    <tr>
+                        {TABLE_HEAD.map((head) => (
+                            <th
+                                key={head}
+                                className="border-b border-red-500 bg-red-50 p-4"
+                            >
+                                <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-normal leading-none opacity-70"
+                                >
+                                    {head}
+                                </Typography>
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((data, index) => {
+                        const isLast = index === data.length - 1;
+                        const classes = isLast ? "p-1" : "p-4 border-b border-blue-50";
+
+                        return (
+                            <tr key={index} className="even:bg-blue-100/50">
+                                <td className={classes}>
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray" 
+                                        className="font-normal"
+                                    >
+                                        {data.id}
+                                    </Typography>
+                                </td>
+                                <td className={classes}>
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal"
+                                    >
+                                        {data.id_fakultas}
+                                    </Typography>
+                                </td>
+                                <td className={classes}>
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal"
+                                    >
+                                        {data.nama_resmi}
+                                    </Typography>
+                                </td>
+                                <td className={classes}>
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal"
+                                    >
+                                        {data.nama_singkat}
+                                    </Typography>
+                                </td>
+
+                                <td className={classes}>
+                                    <Gedung datas={data.id} />
+                                </td>
+
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </Card>
+
+    );
+}
+
+export default App;
---
+++ src/component/gedung.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from 'react'
+import { Card, Typography } from "@material-tailwind/react";
+
+import axios from 'axios'
+
+
+const Gedung = ({ datas }) => {
+
+  const [showModal, setShowModal] = useState(false)
+  const [data, setData] = useState([]);
+
+  const TABLE_HEAD = ["id", "Id Fakultas", "Slug", "Nama Gedung", "Lat", "Long"];
+
+  const fetchPosts = async (datas) => {
+    await axios.get(`https://api.codefm.my.id/gedungs/${datas}`
+    ).then((response) => {
+      setData(response.data)
+
+    }).catch((error) => {
+      console.log(error.message)
+    })
+  }
+
+  useEffect(() => {
+    if (datas) fetchPosts(datas)
+
+  }, [datas])
+
+
+
+  return (
+    <>
+      <button
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Gedung
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-auto">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Gedung
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <table className="align-center w-full min-w-max table-auto text-left">
+                    <thead>
+                      <tr>
+                        {TABLE_HEAD.map((head) => (
+                          <th
+                            key={head}
+                            className="border-b bg-red-500 p-4"
+                          >
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-normal leading-none opacity-70"
+                            >
+                              {head}
+                            </Typography>
+                          </th>
+                        ))}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {data.map((data, index) => {
+                        const isLast = index === data.length - 1;
+                        const classes = isLast ? "p-1" : "p-4 border-b border-blue-50";
+
+                        return (
+                          <tr key={index} className="even:bg-blue-500/50">
+                            <td className={classes}>
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                {data.id}
+                              </Typography>
+                            </td>
+                            <td className={classes}>
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                {data.id_fakultas}
+                              </Typography>
+                            </td>
+                            <td className={classes}>
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                {data.slug}
+                              </Typography>
+                            </td>
+                            <td className={classes}>
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                {data.nama_gedung}
+                              </Typography>
+                            </td>
+                            <td className={classes}>
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                {data.lat}
+                              </Typography>
+                            </td>
+                            <td className={classes}>
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                {data.long}
+                              </Typography>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  )
+}
+
+export default Gedung
